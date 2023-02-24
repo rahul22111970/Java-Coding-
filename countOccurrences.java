33794b8c7ddc90db622e92971bf6ed0081d3e3e3
Rahul Pareek <54419644+rahul22111970@users.noreply.github.com>
2023-02-24 17:57:07 +0530

    Create countOccurrences.java

---
+++ countOccurrences.java
@@ -0,0 +1,9 @@
+public static int countOccurrences(String str, char ch) {
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+        if (str.charAt(i) == ch) {
+            count++;
+        }
+    }
+    return count;
+}
