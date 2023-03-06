10b664666ba31f5a4754f35e6363445107b3f618
Rahul Pareek <54419644+rahul22111970@users.noreply.github.com>
2023-03-06 15:41:14 +0530

    Update SingletonDesignPattern.java

--- SingletonDesignPattern.java
+++ SingletonDesignPattern.java
@@ -31,3 +31,21 @@ public class ConfigurationManager {
         return properties.getProperty(key);
     }
 }
+
+
+//In the above code, the ConfigurationManager class is a singleton class that loads configuration data from a properties file (config.properties). The ConfigurationManager class has a private constructor that loads the configuration data into a Properties object.
+
+//The getInstance() method is a static method that returns the singleton instance of the ConfigurationManager class. If the instance variable is null, the method creates a new instance of the class using the private constructor. If the instance variable is not null, the method simply returns the existing instance.
+
+//The getProperty() method is a public method that returns a configuration property value given a key.
+
+//To use the ConfigurationManager class in other parts of your code, you can call the getInstance() method to obtain the singleton instance, and then call the getProperty() method to retrieve a configuration property value:
+
+
+ConfigurationManager configManager = ConfigurationManager.getInstance();
+String databaseUrl = configManager.getProperty("database.url");
+
+//In the above code, we obtain the singleton instance of the ConfigurationManager class using the getInstance() method, and then retrieve the value of a configuration property (database.url) using the getProperty() method.
+
+
+
