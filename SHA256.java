ab6f1bf3b7f54c0445b8b3c0f32912e0a59d4b05
Rahul Pareek <54419644+rahul22111970@users.noreply.github.com>
2023-03-26 23:30:45 +0530

    Create SHA256.java

---
+++ SHA256.java
@@ -0,0 +1,19 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha256Encryptor {
+    public static String encrypt(String input) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
